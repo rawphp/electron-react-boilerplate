29c526c237b0481a9aa514945508b7acf06d89f1
raw-racing <tom.kaczocha.code@gmail.com>
2019-11-09 11:34:56 +1000

    updated app

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,8 +1,12 @@
-import * as React from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 const App = ({children}) => {
-  return <React.Fragment>{children}</React.Fragment>;
+  return (
+    <>
+    {children}
+    </>
+  );
 };
 
 App.propTypes = {
